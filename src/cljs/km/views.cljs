46b9a1ed644f5e9912283148aad2916f51a261c8
Steven Edwards <steven@stephenwithav.com>
2018-09-23 06:25:40 -0400

    Working as of <2018-09-23 Sun 06:25>.
    re-frame subscriptions have to be assigned to a variable outside of
    the fn body.
    
    Values extracted from the subscriptions must be defined in the fn body
    or they'll never be updated.
    
    Or, simply lookup the keys inline; coder's choice.
    
    Remember to dereference the subscription.

--- src/cljs/km/views.cljs
+++ src/cljs/km/views.cljs
@@ -11,12 +11,14 @@
 (defn main-panel []
   (let [state (re-frame/subscribe [:new-game])]
     (fn []
-      [:div {:id "game-container"}
-       [:div {:id "lang"} "Language: Japanese"]
-       [:div {:id "game-board"}
-       (map make-cell (:cells @state))
-       [:button
-        {:on-click #(re-frame/dispatch [:new-game])}
-        "reset"]
-       ]
-       [:div {:id "game-level"} (:seek @state)]])))
+      (let [cells (:cells @state)
+            seek (:seek @state)]
+        [:div {:id "game-container"}
+         [:div {:id "lang"} "Language: Japanese"]
+         [:div {:id "game-board"}
+          (map make-cell cells)
+          [:button
+           {:on-click #(re-frame/dispatch [:new-game])}
+           "reset"]
+          ]
+         [:div {:id "game-level"} seek]]))))
